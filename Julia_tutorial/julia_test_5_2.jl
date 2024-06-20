401078c6d89c3eb43034c32433ffe0e13f0b9814
Zhengjie Zhang <124875925+zhangzj1209@users.noreply.github.com>
2024-06-20 14:36:21 +0800

    Add files via upload

---
+++ Julia_tutorial/julia_test_5_2.jl
@@ -0,0 +1,9 @@
+function f(x,y)
+	if x > 0
+		return (x-y)
+	elseif x < 0
+		return (x+y)
+	else
+		return x*y
+	end
+end
---
+++ Julia_tutorial/my_program.jl
@@ -0,0 +1,6 @@
+for i = 1 : 9
+	println()
+	for j = 1 : i
+		print(i, "*", j, "=", i*j, " ")
+	end
+end
